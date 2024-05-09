0fd8da0f2d4ad846140f9e1267b59f3c3ac11123
Stepan-Suprunov <ipsuprunovso@mail.ru>
2024-05-10 00:19:08 +0300

    Completed with questions

--- script.js
+++ script.js
@@ -5,107 +5,133 @@ const newItemTitle = newItemForm.querySelector('.add-form-input');
 const taskTemplate = document.querySelector('#task-template').content;
 const newItemTemplate = taskTemplate.querySelector('.todo-list-item');
 
+let todoListArray = JSON.parse(localStorage.getItem('key')) === null ? [] : JSON.parse(localStorage.getItem('key'));
+
 dataRestore();
 
-const completedDelBtn = document.querySelector('.delete-completed-button');
-completedDelBtn.addEventListener('click', function(){
-    let completedListItems = completedList.children; 
-    for (let i = 0; i = completedListItems.length;) {
-        completedListItems[0].remove(); 
-    }
-});
 
 function addTaskHandler (item) {
     const checkbox = item.querySelector('.complete-item');
     checkbox.addEventListener('change', function () {
-        if (this.checked) {
-            let temp = item.cloneNode(true);
-            addTaskHandler(temp);
-            item.remove();
-            completedList.appendChild(temp);
-          } else {
-            let temp = item.cloneNode(true);
-            addTaskHandler(temp);
-            item.remove();
-            todoList.appendChild(temp);
-          }
+        if (this.checked) {          
+            todoListArray.forEach(element => {
+                if (element.id === item.querySelector('p').textContent) {
+                    element.property = true;
+                };
+            });
+        } else {
+            todoListArray.forEach(element => {
+                if (element.id === item.querySelector('p').textContent) {
+                    element.property = false;
+                };
+            });
+        };
+
+        refreshLocalStorage ();
     });
 
     const delBtn = item.querySelector('.delete-item');
     delBtn.addEventListener('click', function() {
-        item.remove();
-    });
+        todoListArray.forEach(element => {
+            // if (element.id === item.querySelector('p').textContent) {
+            //     element.remove();                                               Почему не работает .remove() ?
+            // };
 
-    const newListBtn = document.querySelector('.new-list-button');
-    newListBtn.addEventListener('click', function() {
-        item.remove();
+            let temp = [];
+            todoListArray.forEach(element => {
+                if (element.id !== item.querySelector('p').textContent) {
+                    temp.push(element);
+                }
+
+                todoListArray = temp;                                              // Пришлось сменить todoListArray на let
+            });
+        });
+
+        refreshLocalStorage ();
     });
 };
 
-function refreshLocalStorage () {
+
+function addElement () {
     const myElement = {
         todo: String,
         property: Boolean,
-        id: Date()
+        id: Date()                                                    // Почему программа перестаёт работать при исмользовании Date.now(); ?
     };
-    const myArray = JSON.parse(localStorage.getItem('key')) === null ? [] : JSON.parse(localStorage.getItem('key'));
-    //console.log(JSON.stringify(myArray));
-    let myArrayResult = [];
+
     myElement.todo = newItemTitle.value;
     myElement.property = false;
     myElement.id = Date();
-    //console.log(JSON.stringify(myElement));
-    myArray.push(myElement);
-    //console.log(JSON.stringify(myArray));
-    localStorage.setItem('key', JSON.stringify(myArray));
-    myArrayResult = JSON.parse(localStorage.getItem('key'));
-    //console.log(JSON.stringify(myArrayResult));
-    return myArrayResult;
+
+    todoListArray.push(myElement);
+
+    refreshLocalStorage ();
+
+    return myElement;
 };
 
+
+function refreshLocalStorage () {
+    localStorage.setItem('key', JSON.stringify(todoListArray));
+    location.reload ();
+};
+
+
 function dataRestore () {
-    const data = refreshLocalStorage();
-    data.forEach(element => {
-        const taskText = element.todo;
+    todoListArray.forEach(element => {
         const task = newItemTemplate.cloneNode(true);
-        const taskDescription = task.querySelector('span');
-        taskDescription.textContent = taskText;
+        task.querySelector('span').textContent = element.todo;
+        task.querySelector('p').textContent = element.id;
+
+        //task.querySelector('.complete-item').checked = element.property;    Не понятно, почему не срабатывает присваивание булева значения.
+        
+        if (element.property === false) {
+            todoList.appendChild(task);
+        } else {
+            task.querySelector('.complete-item').checked = true;
+            completedList.appendChild(task);
+        }
+        
         addTaskHandler(task);
-        todoList.appendChild(task);
     });
 }
 
+
 newItemForm.addEventListener('submit', function() {
-    refreshLocalStorage();
-    const taskText = newItemTitle.value;
+    const element = addElement ();
     const task = newItemTemplate.cloneNode(true);
-    const taskDescription = task.querySelector('span');
-    taskDescription.textContent = taskText;    
+    task.querySelector('span').textContent = element.todo; 
+    task.querySelector('p').textContent = element.id;                             // Вопросик. Как избавиться от этого костыля?
     addTaskHandler(task);
     todoList.appendChild(task);
     newItemTitle.value = '';
 });
 
 
-    
+const newListBtn = document.querySelector('.new-list-button');
+newListBtn.addEventListener('click', function() {
+    todoListArray.length = 0;
+    refreshLocalStorage ();
+});
 
 
-// const todoList = document.querySelector('.todo-list');
-// const completedList = document.querySelector('.completed-list');
-// const newItemForm = document.querySelector('.add-form');
-// const newItemTitle = newItemForm.querySelector('.add-form-input');
-// const taskTemplate = document.querySelector('#task-template').content;
-// const newItemTemplate = taskTemplate.querySelector('.todo-list-item');
+const completedDelBtn = document.querySelector('.delete-completed-button');
+completedDelBtn.addEventListener('click', function(){
 
-// const addTaskHandler = function(item) {
-//     newItemForm.addEventListener('submit', function() {
-//         const taskText = newItemTitle.value;
-//         const task = newItemTemplate.cloneNode(true);
-//         const taskDescription = task.querySelector('span');
-//         taskDescription.textContent = taskText;
-//         localStorage.setItem('incomplete', task.toString());
-//         addTaskHandler(task);
-//         todoList.appendChild(task);
-//         newItemTitle.value = '';
-//     });
-// };
+    // todoListArray.forEach(element => {
+    //     if (element.property) {                                                Почему не работает .remove() ?
+    //         element.remove();
+    //     }
+    // });
+
+    let temp = [];
+    todoListArray.forEach(element => {
+        if (!element.property) {
+            temp.push(element);
+        }
+
+        todoListArray = temp;                                                 // Пришлось сменить todoListArray на let
+    });
+
+    refreshLocalStorage ();
+});
